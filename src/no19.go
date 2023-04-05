03d357a27bd93edf8598ce186c6e1626d2af7669
Leon Song <songliu0608@qq.com>
2023-04-06 00:18:17 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ src/no19.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/*
+*
+19. 删除链表的倒数第 N 个结点
+https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+*/
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	length := countNthFromEnd(head, n)
+
+	if length == n-1 {
+		return head.Next
+	}
+
+	return head
+}
+
+func countNthFromEnd(head *ListNode, n int) int {
+
+	var length int
+
+	if head != nil && head.Next == nil {
+		length = 0
+	} else {
+		length = countNthFromEnd(head.Next, n) + 1
+	}
+
+	if length == n {
+		head.Next = head.Next.Next
+	}
+
+	return length
+}
+
+func main() {
+
+	node1 := &ListNode{Val: 1}
+	node2 := &ListNode{Val: 2}
+	node3 := &ListNode{Val: 3}
+	node4 := &ListNode{Val: 4}
+	node5 := &ListNode{Val: 5}
+
+	node1.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+	node4.Next = node5
+
+	ans := removeNthFromEnd(node1, 5)
+
+	fmt.Print(ans)
+
+}
--- src/no2.go
+++ src/no2.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
 
 /*
 *
