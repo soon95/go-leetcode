d1a77a08ec84df8b758ceb07c257f0da37868abb
Leon Song <songliu0608@qq.com>
2023-04-23 23:24:45 +0800

    优化一下思路，使代码更加简洁一些

--- src/problems/no33/main.go
+++ src/problems/no33/main.go
@@ -66,12 +66,52 @@ func search(nums []int, target int) int {
 	return -1
 }
 
+func searchV2(nums []int, target int) int {
+
+	start := 0
+	end := len(nums) - 1
+
+	for start <= end {
+
+		middle := (start + end) / 2
+
+		if nums[middle] == target {
+			return middle
+		}
+
+		if nums[start] <= nums[middle] {
+			// 如果前面有序
+
+			if nums[start] <= target && nums[middle] > target {
+				// 如果目标在前面的有序区间内
+				end = middle - 1
+			} else {
+				start = middle + 1
+			}
+
+		} else {
+			// 如果后面有序
+
+			if nums[middle] < target && nums[end] >= target {
+				// 如果目标在后面的有序区间内
+				start = middle + 1
+			} else {
+				end = middle - 1
+			}
+
+		}
+
+	}
+
+	return -1
+}
+
 func main() {
 
 	nums := []int{4, 5, 6, 7, 0, 1, 2}
 
-	target := 0
+	target := 3
 
-	fmt.Print(search(nums, target))
+	fmt.Print(searchV2(nums, target))
 
 }
