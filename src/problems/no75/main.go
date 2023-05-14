0a61a499dce5d270302b1483fd536f00d1554fd1
Leon Song <songliu0608@qq.com>
2023-05-14 22:02:29 +0800

    75. 颜色分类(优化一下)

--- src/problems/no75/main.go
+++ src/problems/no75/main.go
@@ -36,11 +36,44 @@ func sortColors(nums []int) {
 
 }
 
+func sortColorsV2(nums []int) {
+
+	oneStart := 0
+	twoStart := 0
+
+	for i := 0; i < len(nums); i++ {
+
+		if nums[i] == 0 {
+
+			if oneStart == twoStart {
+				exchange(nums, i, oneStart)
+			} else {
+				exchange(nums, i, oneStart)
+				exchange(nums, i, twoStart)
+			}
+
+			oneStart++
+			twoStart++
+		} else if nums[i] == 1 {
+			exchange(nums, i, twoStart)
+			twoStart++
+		}
+
+	}
+}
+
+func exchange(nums []int, i, j int) {
+	temp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = temp
+}
+
 func main() {
 
-	nums := []int{2, 1}
+	//nums := []int{2, 1}
+	nums := []int{2, 0, 2, 1, 1, 0}
 
-	sortColors(nums)
+	sortColorsV2(nums)
 
 	fmt.Printf("%v", nums)
 
